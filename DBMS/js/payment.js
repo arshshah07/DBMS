d936ee35cf4d956dde4ce3d9de5b378dd18c6c3d
Arsh Shah <52788496+arshshah07@users.noreply.github.com>
2022-04-07 21:45:50 +0530

    Add files via upload

---
+++ DBMS/js/payment.js
@@ -0,0 +1,24 @@
+var btn2 = document.querySelector('.submit_payment_btn')
+var forms = document.getElementsByClassName('needs-validation');
+
+
+var validation = Array.prototype.filter.call(forms, function(form) {
+      btn2.addEventListener('click', function(e) {
+        if (form.checkValidity() === false) {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+        else{
+        window.location.replace('thankyou.html')
+        e.preventDefault();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+
+// btn2.addEventListener("click", function(e){
+
+//   window.location.replace('thankyou.html')
+//   e.preventDefault();
+// })
+
